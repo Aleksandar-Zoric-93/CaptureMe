cf37555eeeff5d7edeea1b2246b35ce52007dfa0
Alex Zoric <alex.zoric@diomac.com>
2019-11-28 11:54:58 +0000

    Initial Commit

---
+++ WindowsFormsApp1/Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.captureFrame = new System.Windows.Forms.PictureBox();
+            this.streamBtn = new System.Windows.Forms.Button();
+            this.snapshotBtn = new System.Windows.Forms.Button();
+            this.Camera_Selection = new System.Windows.Forms.ComboBox();
+            this.cameraLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.captureFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureFrame
+            // 
+            this.captureFrame.ErrorImage = global::WindowsFormsApp1.Properties.Resources.giphy;
+            this.captureFrame.InitialImage = global::WindowsFormsApp1.Properties.Resources.giphy;
+            this.captureFrame.Location = new System.Drawing.Point(0, -2);
+            this.captureFrame.Name = "captureFrame";
+            this.captureFrame.Size = new System.Drawing.Size(802, 296);
+            this.captureFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.captureFrame.TabIndex = 0;
+            this.captureFrame.TabStop = false;
+            // 
+            // streamBtn
+            // 
+            this.streamBtn.BackColor = System.Drawing.Color.SeaGreen;
+            this.streamBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.streamBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.streamBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.streamBtn.Location = new System.Drawing.Point(12, 300);
+            this.streamBtn.Name = "streamBtn";
+            this.streamBtn.Size = new System.Drawing.Size(252, 118);
+            this.streamBtn.TabIndex = 1;
+            this.streamBtn.Text = "Start";
+            this.streamBtn.UseVisualStyleBackColor = false;
+            this.streamBtn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // snapshotBtn
+            // 
+            this.snapshotBtn.BackColor = System.Drawing.Color.CadetBlue;
+            this.snapshotBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.snapshotBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snapshotBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.snapshotBtn.Location = new System.Drawing.Point(536, 300);
+            this.snapshotBtn.Name = "snapshotBtn";
+            this.snapshotBtn.Size = new System.Drawing.Size(252, 118);
+            this.snapshotBtn.TabIndex = 2;
+            this.snapshotBtn.Text = "Take Snapshot";
+            this.snapshotBtn.UseVisualStyleBackColor = false;
+            this.snapshotBtn.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Camera_Selection
+            // 
+            this.Camera_Selection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Camera_Selection.FormattingEnabled = true;
+            this.Camera_Selection.Location = new System.Drawing.Point(83, 424);
+            this.Camera_Selection.Name = "Camera_Selection";
+            this.Camera_Selection.Size = new System.Drawing.Size(181, 21);
+            this.Camera_Selection.TabIndex = 3;
+            // 
+            // cameraLbl
+            // 
+            this.cameraLbl.AutoSize = true;
+            this.cameraLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cameraLbl.Location = new System.Drawing.Point(12, 425);
+            this.cameraLbl.Name = "cameraLbl";
+            this.cameraLbl.Size = new System.Drawing.Size(65, 20);
+            this.cameraLbl.TabIndex = 4;
+            this.cameraLbl.Text = "Camera";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cameraLbl);
+            this.Controls.Add(this.Camera_Selection);
+            this.Controls.Add(this.snapshotBtn);
+            this.Controls.Add(this.streamBtn);
+            this.Controls.Add(this.captureFrame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "CaptureMe";
+            ((System.ComponentModel.ISupportInitialize)(this.captureFrame)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox captureFrame;
+        private System.Windows.Forms.Button streamBtn;
+        private System.Windows.Forms.Button snapshotBtn;
+        private System.Windows.Forms.ComboBox Camera_Selection;
+        private System.Windows.Forms.Label cameraLbl;
+    }
+}
+
---
+++ WindowsFormsApp1/Main.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Drawing.Imaging;
+using System.Threading;
+using DirectShowLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class Main : Form
+    {
+        // Create class-level accesible variables
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        bool isCameraRunning = false;
+        int CameraDevice = 0;
+        int runningCamera;
+        Video_Device[] WebCams;
+
+
+        // Declare required methods
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            try
+            {
+                frame = new Mat();
+                capture = new VideoCapture();
+                capture.Open(CameraDevice);
+                while (isCameraRunning)
+                {
+                    capture.Read(frame);
+                    image = BitmapConverter.ToBitmap(frame);
+                    captureFrame.Image = image;
+                    image = null;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No camera found on device", "Start Stream",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (streamBtn.InvokeRequired)
+                {
+                    streamBtn.Invoke(new MethodInvoker(delegate { streamBtn.Text = "Start"; }));
+                }
+            }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //-> Find systems cameras with DirectShow.Net dll
+            DsDevice[] _SystemCamereas = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+            WebCams = new Video_Device[_SystemCamereas.Length];
+            for (int i = 0; i < _SystemCamereas.Length; i++)
+            {
+                WebCams[i] = new Video_Device(i, _SystemCamereas[i].Name, _SystemCamereas[i].ClassID); //fill web cam array
+                Camera_Selection.Items.Add(WebCams[i].ToString()[1]);
+            }
+            if (Camera_Selection.Items.Count > 0)
+            {
+                Camera_Selection.SelectedIndex = CameraDevice; //Set the selected device the default  
+
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (streamBtn.Text.Equals("Start"))
+            {
+                CaptureCamera();
+                streamBtn.Text = "Stop";
+                isCameraRunning = true;
+            }
+            else
+            {
+                capture.Release();
+                streamBtn.Text = "Start";
+                isCameraRunning = false;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (isCameraRunning)
+            {
+                try
+                {
+                    // Take snapshot of the current image generate by OpenCV in the Picture Box
+                    Bitmap snapshot = new Bitmap(captureFrame.Image);
+                    captureFrame.Image.Dispose();
+                    // Save in some directory
+                    // in this example, we'll generate a random filename e.g 47059681-95ed-4e95-9b50-320092a3d652.png
+                    // snapshot.Save(@"C:\Users\sdkca\Desktop\mysnapshot.png", ImageFormat.Png);
+                    //snapshot.Save(string.Format(@"C:\Users\Diomac\Desktop\testSnapshot.png", Guid.NewGuid()), ImageFormat.Png);
+
+                    saveSnapshot();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Device must be streaming to take a snapshot", "Take Snapshot",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Device must be streaming to take a snapshot", "Take Snapshot",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void saveSnapshot()
+        {
+            try
+            {
+                var saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Title = @"Save Your Photo";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    captureFrame.Image.Save(saveFileDialog.FileName + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                    MessageBox.Show(@"Saved!");
+                }
+            }catch(Exception)
+            {
+                MessageBox.Show("Device must be streaming to take a snapshot", "Take Snapshot",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Sets up the _capture variable with the selected camera index
+        /// </summary>
+        /// <param name="Camera_Identifier"></param>
+        private void SetupCapture(int Camera_Identifier)
+        {
+            //update the selected device
+            CameraDevice = Camera_Identifier;
+
+            //Dispose of Capture if it was created before
+            if (capture != null) capture.Dispose();
+            try
+            {
+                //Set up capture device
+                capture = new VideoCapture(CameraDevice);
+                CaptureCamera();
+            }
+
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+    }
+}
